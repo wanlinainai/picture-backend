c9af1b008e9556af0f0568dc24ba3d75222be7d5
zxh <zxh15166423670@163.com>
2025-04-16 22:37:33 +0800

    fix JS precision loss problem

---
+++ src/main/java/com/yupi/yupicturebackend/config/JsonConfig.java
@@ -0,0 +1,33 @@
+package com.yupi.yupicturebackend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * @ClassName: JsonConfig
+ * @Author: zxh
+ * @Date: 2025/4/16 22:33
+ * @Description: 解决前端JS处理精度问题
+ */
+@JsonComponent
+public class JsonConfig {
+
+    /**
+     * 添加Long转 json精度丢失问题
+     * @param builder
+     * @return
+     */
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        return objectMapper;
+    }
+}
