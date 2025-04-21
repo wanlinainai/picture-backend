bf7c9818feb468e751bf9c4c41583de6f61c2d1e
zxh <zxh15166423670@gmail.com>
2025-04-22 01:20:32 +0800

    新增模板类，增加本地上传的实现类

---
+++ src/main/java/com/yupi/yupicturebackend/manager/upload/FilePictureUpload.java
@@ -0,0 +1,46 @@
+package com.yupi.yupicturebackend.manager.upload;
+
+import cn.hutool.core.io.FileUtil;
+import com.yupi.yupicturebackend.exception.ErrorCode;
+import com.yupi.yupicturebackend.exception.ThrowUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @ClassName: FilePictureUpload
+ * @Author: zxh
+ * @Date: 2025/4/22 01:15
+ * @Description: 本地图片上传
+ */
+@Service
+public class FilePictureUpload extends PictureUploadTemplate{
+
+    @Override
+    protected void validPicture(Object inputSource) {
+        MultipartFile multipartFile = (MultipartFile) inputSource;
+        ThrowUtils.throwIf(multipartFile == null, ErrorCode.PARAMS_ERROR, "文件不能为空");
+        // 校验文件大小
+        long fileSize = multipartFile.getSize();
+        final long ONE_M = 1024 * 1024L;
+        ThrowUtils.throwIf(fileSize > 2 * ONE_M, ErrorCode.PARAMS_ERROR, "文件大小不能超过 2M");
+        // 校验文件后缀
+        String fileSuffix = FileUtil.getSuffix(multipartFile.getOriginalFilename());
+        // 允许上传文件后缀
+        final List<String> ALLOW_FORMAT_LIST = Arrays.asList("jpeg", "jpg", "png", "webp");
+        ThrowUtils.throwIf(!ALLOW_FORMAT_LIST.contains(fileSuffix), ErrorCode.PARAMS_ERROR, "不支持的文件后缀格式");
+    }
+
+    @Override
+    protected String getOriginFileName(Object inputSource) {
+        return "";
+    }
+
+    @Override
+    protected void processFile(Object inputSource, File file) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/yupi/yupicturebackend/manager/upload/PictureUploadTemplate.java
@@ -0,0 +1,120 @@
+package com.yupi.yupicturebackend.manager.upload;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.RandomUtil;
+import com.qcloud.cos.model.PutObjectResult;
+import com.qcloud.cos.model.ciModel.persistence.ImageInfo;
+import com.yupi.yupicturebackend.config.CosClientConfig;
+import com.yupi.yupicturebackend.exception.BussinessException;
+import com.yupi.yupicturebackend.exception.ErrorCode;
+import com.yupi.yupicturebackend.manager.CosManager;
+import com.yupi.yupicturebackend.model.dto.file.UploadPictureResult;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.util.Date;
+
+/**
+ * @ClassName: PictureUploadTemplate
+ * @Author: zxh
+ * @Date: 2025/4/22 00:59
+ * @Description: 图片上传模版
+ */
+@Slf4j
+public abstract class PictureUploadTemplate {
+    @Resource
+    private CosManager cosManager;
+
+    @Resource
+    private CosClientConfig cosClientConfig;
+
+    /**
+     * 模版方法
+     */
+    public final UploadPictureResult uploadPicture(Object inputSource, String uploadPathPrefix) {
+        // 校验图片
+        validPicture(inputSource);
+        // 图片上传地址
+        String uuid = RandomUtil.randomString(16);
+        String originFileName = getOriginFileName(inputSource);
+        String uploadFileName = String.format("%s_%s.%s", DateUtil.formatDate(new Date()), uuid, FileUtil.getSuffix(originFileName));
+        String uploadPath = String.format("/%s/%s", uploadPathPrefix, uploadFileName);
+        File file = null;
+        try {
+            // 创建临时文件
+            file = File.createTempFile(uploadPath, null);
+            processFile(inputSource, file);
+
+            PutObjectResult putObjectResult = cosManager.putPictureObject(uploadPath, file);
+            ImageInfo imageInfo = putObjectResult.getCiUploadResult().getOriginalInfo().getImageInfo();
+
+            return buildResult(originFileName, file, uploadPath, imageInfo);
+        }catch (Exception e) {
+            log.error("图片上传失败," + e);
+            throw new BussinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
+        }finally {
+            deleteTempFile(file);
+        }
+    }
+
+    /**
+     * 校验输入源
+     * @param inputSource
+     */
+    protected abstract void validPicture(Object inputSource);
+
+    /**
+     * 获取输入源的原始文件名
+     * @param inputSource
+     * @return
+     */
+    protected abstract String getOriginFileName(Object inputSource);
+
+    /**
+     * 处理输入源并生成文件本地临时文件
+     * @param inputSource
+     * @param file
+     * @throws Exception
+     */
+    protected abstract void processFile(Object inputSource, File file) throws Exception;
+
+    /**
+     * 封装返回结果
+     * @param originFileName
+     * @param file
+     * @param uploadPath
+     * @param imageInfo
+     * @return
+     */
+    private UploadPictureResult buildResult(String originFileName, File file, String uploadPath, ImageInfo imageInfo) {
+        UploadPictureResult uploadPictureResult = new UploadPictureResult();
+        int picWidth = imageInfo.getWidth();
+        int picHeight = imageInfo.getHeight();
+        double picScale  = NumberUtil.round(picWidth * 1.0 / picHeight, 2).doubleValue();
+
+        uploadPictureResult.setPicName(FileUtil.mainName(originFileName));
+        uploadPictureResult.setUrl(cosClientConfig.getHost() + "/" + uploadPath);
+        uploadPictureResult.setPicWidth(picWidth);
+        uploadPictureResult.setPicHeight(picHeight);
+        uploadPictureResult.setPicSize(FileUtil.size(file));
+        uploadPictureResult.setPicScale(picScale);
+        uploadPictureResult.setPicFormat(imageInfo.getFormat());
+        return uploadPictureResult;
+    }
+
+    /**
+     * 删除临时文件
+     */
+    public void deleteTempFile(File file) {
+        if (file == null) {
+            return;
+        }
+        boolean deleteResult = file.delete();
+        if (!deleteResult) {
+            log.error("file delete error, filePath = {}", file.getAbsoluteFile());
+        }
+    }
+}
